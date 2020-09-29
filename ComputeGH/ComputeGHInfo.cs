e0597f504cd17de9b3b5505bd1913a04dcedca5a
ocni-dtu <lxE1eX7YsEAWSQ4K>
2020-09-29 22:20:52 +0200

    working on automating builds

--- ComputeGH/ComputeGHInfo.cs
+++ ComputeGH/ComputeGHInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using Grasshopper.Kernel;
 
 namespace ComputeGH
@@ -10,9 +11,15 @@ namespace ComputeGH
         {
             get
             {
-                return "Compute";
+                return "ProceduralCS";
             }
         }
+
+        public override string Version
+        {
+            get { return NextVersion(); }
+        }
+
         public override Bitmap Icon
         {
             get
@@ -52,6 +59,28 @@ namespace ComputeGH
                 //Return a string representing your preferred contact details.
                 return "info@procedural.build";
             }
+        }
+
+        private static string GetCurrentVersion()
+        {
+            return "0.0.0";
+        }
+        private static string GetNextBuildVersion()
+        {
+            var currentVersion = GetCurrentVersion().Split('.').Select(x => Convert.ToInt32(x)).ToList();
+            var date = new DateTime().ToString("yyyy.m").Split('.').Select(x => Convert.ToInt32(x)).ToList();
+            if (date[0] > currentVersion[0] || date[1] > currentVersion[1])
+            {
+                return "0";
+            }
+            return (currentVersion[2] + 1).ToString();
+        }
+
+        private static string NextVersion()
+        {
+            var buildNumber = GetNextBuildVersion();
+            var date = new DateTime().ToString("yyyy.m");
+            return $"{date}.${buildNumber}";
         }
     }
 }
---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+﻿#& "C:\Program Files\Rhino 6\System\yak.exe" login
+$yak = "C:\Program Files\Rhino 7 WIP\System\yak.exe"
+$name = & $yak search ProceduralCS
+$name -match "[0-9.]+"
+$currentVersion = $Matches[0]
+$year, $month, [int]$buildNumber = $currentVersion.split('.')
+$YearMonth = Get-Date -Format "yyyy.M"
+$buildNumber = $buildNumber + 1
+$newVersion = "$($YearMonth).$($buildNumber)"
+cd "$($PSScriptRoot)\dist"
+& $yak build
