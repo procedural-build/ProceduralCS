fbcf6503ec4ebe85c2a46b17b41828eb7c0f7585
ocni-dtu <lxE1eX7YsEAWSQ4K>
2021-01-11 15:35:07 +0100

    fixed release

--- componentChanges.ps1
+++ componentChanges.ps1
@@ -1,4 +1,11 @@
-﻿git fetch --prune --unshallow
+﻿# Bump version
+
+Get-ChildItem dist -Filter *.yak | ForEach-Object -Process {$_[0] -match "[0-9.]+"}
+$version = $Matches[0]
+echo RELEASE_VERSION=$version | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+
+# Create Change log for components
+git fetch --prune --unshallow
 $tag =  git describe --abbrev=0 --tags
 $changes = git diff --compact-summary $tag ComputeGH/CFD ComputeGH/Utils
 
@@ -21,7 +28,6 @@ $newComponents
 echo "UPDATED COMPONENTS"
 $changedComponents
 
-$version = "2021.1.1"
 $releaseFile = ".\.github\releases\release.md"
 $release = Get-Content $releaseFile
 $release = $release.Replace("{{ NEW_COMPONENTS }}", $newComponents)
