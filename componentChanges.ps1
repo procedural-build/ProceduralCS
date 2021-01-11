e90ceb078147005e4914fcdce21cc7aab48b6a5f
ocni-dtu <lxE1eX7YsEAWSQ4K>
2021-01-11 15:20:46 +0100

    trying with body_path for release

--- componentChanges.ps1
+++ componentChanges.ps1
@@ -1,4 +1,4 @@
-﻿git fetch --prune --unshallow
+﻿#git fetch --prune --unshallow
 $tag =  git describe --abbrev=0 --tags
 $changes = git diff --compact-summary $tag ComputeGH/CFD ComputeGH/Utils
 
@@ -7,11 +7,11 @@ $changedComponents = ""
 foreach ($line in $changes){
     if ($line.Contains("new")){
         $line = $line -replace "ComputeGH\/(Utils|CFD)\/GH",""
-        $newComponents += ' * ' + ($line -replace ".cs.*", "") + '%0A'
+        $newComponents += " * " + ($line -replace ".cs.*", "") + "`n"
     }
     if ($line.Contains("ComputeGH")) {
         $line = $line -replace "ComputeGH\/(Utils|CFD)\/GH",""
-        $changedComponents += ' * ' + ($line -replace ".cs.*", "") + '%0A'
+        $changedComponents += " * " + ($line -replace ".cs.*", "") + "`n"
     }
     
 }
@@ -21,5 +21,14 @@ $newComponents
 echo "UPDATED COMPONENTS"
 $changedComponents
 
-echo NEW_COMPONENTS=$newComponents | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-echo UPDATED_COMPONENTS=$changedComponents | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+$version = "2021.1.1"
+$releaseFile = ".\.github\releases\release1.md"
+$release = Get-Content $releaseFile
+$release = $release.Replace("{{ NEW_COMPONENTS }}", $newComponents)
+$release = $release.Replace("{{ UPDATED_COMPONENTS }}", $changedComponents)
+$release = $release.Replace("{{ RELEASE_VERSION }}", $version)
+
+echo $release | Out-File -FilePath $releaseFile -Encoding utf8
+
+#echo NEW_COMPONENTS=$newComponents | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+#echo UPDATED_COMPONENTS=$changedComponents | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
