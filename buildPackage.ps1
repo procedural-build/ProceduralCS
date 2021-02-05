f4386c5a55fdf1ee0c319d6bf9c2f6cb90b65190
ocni-dtu <lxE1eX7YsEAWSQ4K>
2021-02-05 14:21:52 +0100

    updated github actions to also build Rhino 7 packages

---
+++ buildPackage.ps1
@@ -0,0 +1,8 @@
+﻿# BUILD YAK PACKAGE
+cd $env:DIST_PATH\
+..\$env:YAK_EXE build --platform win
+
+# COPY TO RHINO 7
+$rh6 = (Get-ChildItem . -Filter *.yak)[0]
+$rh7 = $rh6 -replace "rh6_20", "rh7_0"
+Rename-Item -Path $rh6 -NewName $rh7
