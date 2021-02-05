493ac24d6cff5b69e8ebc84d90487fd46ffc7c2d
ocni-dtu <lxE1eX7YsEAWSQ4K>
2021-02-05 16:03:19 +0100

    updated build packages

--- buildPackage.ps1
+++ buildPackage.ps1
@@ -5,4 +5,7 @@ cd $env:DIST_PATH\
 # COPY TO RHINO 7
 $rh6 = (Get-ChildItem . -Filter *.yak)[0]
 $rh7 = $rh6 -replace "rh6_20", "rh7_0"
-Rename-Item -Path $rh6 -NewName $rh7
+Copy-Item -Path $rh6 -Destination $rh7
+
+# ECHO FILES
+Get-ChildItem . -Filter *.yak
