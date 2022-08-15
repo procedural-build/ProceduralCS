044be2caa3b63f76651b71dfe04cd863c11667f4
Dion Jansen <dionjansen@wlnss.com>
2022-08-15 19:41:23 +0200

    fix(computegh): #3 use task capable comp for login (#15)
    fix: #3

--- ComputeGH/General/GHLogin.cs
+++ ComputeGH/General/GHLogin.cs
@@ -1,18 +1,21 @@
 ﻿using ComputeCS.types;
-using ComputeCS.utils.Cache;
-using ComputeCS.utils.Queue;
 using ComputeGH.Grasshopper.Utils;
 using ComputeGH.Properties;
 using Grasshopper.Kernel;
 using System;
 using System.Drawing;
-using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace ComputeCS.Grasshopper
 {
-    public class ComputeLogin : PB_Component
+    public class LoginResult
     {
+        public Inputs Value { get; set; }
+        public Exception Errors { get; set; }
+    }
+    public class ComputeLogin : PB_TaskCapableComponent<LoginResult>
+    {
+
         /// <summary>
         /// Initializes a new instance of the computeLogin class.
         /// </summary>
@@ -46,98 +49,6 @@ namespace ComputeCS.Grasshopper
         }
 
         /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            string username = null;
-            string password = null;
-            var url = "https://compute.procedural.build";
-            var retry = false;
-
-            if (!DA.GetData(0, ref username)) return;
-            if (!DA.GetData(1, ref password)) return;
-            DA.GetData(2, ref url);
-            DA.GetData(3, ref retry);
-
-            var client = new ComputeClient(url);
-
-            if (retry)
-            {
-                StringCache.ClearCache();
-            }
-
-            //Async Execution
-            var cacheKey = username + password + url;
-            var cachedTokens = StringCache.getCache(cacheKey);
-            DA.DisableGapLogic();
-            if (cachedTokens == null)
-            {
-                var queueName = "login";
-                // Get queue lock
-                var queueLock = StringCache.getCache(queueName);
-                if (queueLock != "true")
-                {
-                    StringCache.setCache(queueName, "true");
-                    QueueManager.addToQueue(queueName, () =>
-                    {
-                        try
-                        {
-                            var results = client.Auth(username, password);
-
-                            if (results.ErrorMessages != null)
-                            {
-                                StringCache.setCache(cacheKey, "error");
-                                throw new Exception(results.ErrorMessages.First());
-                            }
-                            if (results.ErrorMessages == null)
-                            {
-                                StringCache.ClearCache();
-                                cachedTokens = results.ToJson();
-                                StringCache.setCache(cacheKey, cachedTokens);
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            StringCache.setCache(InstanceGuid.ToString(), e.Message);
-                        }
-
-                        ExpireSolutionThreadSafe(true);
-                        Thread.Sleep(2000);
-                        StringCache.setCache(queueName, "");
-                    });
-                }
-            }
-
-
-            // Read from Cache
-            var errors = StringCache.getCache(InstanceGuid.ToString());
-            if (errors != null)
-            {
-                if (errors.Contains("(401) Unauthorized"))
-                {
-                    errors = "Could not login with the provided credentials. Try again.";
-                }
-
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, errors);
-            }
-
-            var tokens = new AuthTokens();
-            if (cachedTokens != null)
-            {
-                tokens = tokens.FromJson(cachedTokens);
-                var output = new Inputs
-                {
-                    Auth = tokens,
-                    Url = url
-                };
-                DA.SetData(0, output.ToJson());
-            }
-
-        }
-
-        /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
         protected override Bitmap Icon => Resources.IconLicense;
@@ -146,5 +57,59 @@ namespace ComputeCS.Grasshopper
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
         public override Guid ComponentGuid => new Guid("3bcb2f6e-7c64-41b2-8086-6b1ddd6e80ee");
+
+        public override Task<LoginResult> CreateTask(IGH_DataAccess DA)
+        {
+            string username = null;
+            string password = null;
+            var url = "https://compute.procedural.build";
+            var retry = false;
+
+            if (!DA.GetData(0, ref username)) return DefaultTask();
+            if (!DA.GetData(1, ref password)) return DefaultTask();
+            DA.GetData(2, ref url);
+            DA.GetData(3, ref retry);
+
+            return System.Threading.Tasks.Task.Run(
+                () => DoAuth(url, username, password), CancelToken);
+        }
+
+        public override void SetOutputData(IGH_DataAccess DA, LoginResult result)
+        {
+            if (result.Errors != null)
+            {
+                var errorMessage = result.Errors.Message.Contains("(401) Unauthorized")
+                    ? "Could not login with the provided credentials. Try again."
+                    : result.Errors.Message;
+
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, errorMessage);
+                return;
+            }
+
+            DA.SetData(0, result.Value.ToJson());
+        }
+
+        private LoginResult DoAuth(string url, string username, string password)
+        {
+            var client = new ComputeClient(url);
+            try
+            {
+                return new LoginResult
+                {
+                    Value = new Inputs
+                    {
+                        Auth = client.Auth(username, password),
+                        Url = url
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                return new LoginResult
+                {
+                    Errors = e
+                };
+            };
+        }
     }
 }
