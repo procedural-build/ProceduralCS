488a5012ebb3f752199e6bb05c019c63165141c2
Dion Jansen <dionjansen@wlnss.com>
2022-08-15 19:28:26 +0200

    fix(computegh): #2 add task capable base class (#14)
    fix: #2

---
+++ ComputeGH/GrasshopperUtils/PB_TaskCapableComponent.cs
@@ -0,0 +1,44 @@
+﻿using Grasshopper.Kernel;
+using System.Threading.Tasks;
+
+namespace ComputeGH.Grasshopper.Utils
+{
+    public abstract class PB_TaskCapableComponent<T> : GH_TaskCapableComponent<T>
+    {
+        protected PB_TaskCapableComponent(
+            string name,
+            string nickname,
+            string description,
+            string category,
+            string subCategory) : base(name, nickname, description, category, subCategory)
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            if (InPreSolve)
+            {
+                // First pass; collect input data and  Queue up the task
+                TaskList.Add(CreateTask(DA));
+                return;
+            }
+
+            if (!GetSolveResults(DA, out T result))
+            {
+                // Compute right here; collect input data and run task
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Parallel processing must be turned on!");
+            }
+
+            // Set output data
+            if (result != null)
+                SetOutputData(DA, result);
+            else
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No result was solved");
+        }
+
+        public abstract Task<T> CreateTask(IGH_DataAccess DA);
+        public abstract void SetOutputData(IGH_DataAccess DA, T result);
+
+        protected Task<T> DefaultTask() => new Task<T>(() => default);
+    }
+}
