dfecf5806e5c7475dae075b63db258f0d0652cf7
Dion Jansen <dionjansen@wlnss.com>
2022-08-15 19:41:49 +0200

    fix(computegh): #4 use task capable comp for projecttask (#16)
    fix: #4

--- ComputeGH/General/GHProjectTask.cs
+++ ComputeGH/General/GHProjectTask.cs
@@ -1,6 +1,4 @@
 ﻿using ComputeCS.Components;
-using ComputeCS.utils.Cache;
-using ComputeCS.utils.Queue;
 using ComputeGH.Grasshopper.Utils;
 using ComputeGH.Properties;
 using Grasshopper.Kernel;
@@ -9,11 +7,16 @@ using System.Activities;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace ComputeCS.Grasshopper
 {
-    public class ComputeProjectTask : PB_Component
+    public class ProjectTaskResult
+    {
+        public string Value { get; set; }
+        public Exception Errors { get; set; }
+    }
+    public class ComputeProjectTask : PB_TaskCapableComponent<ProjectTaskResult>
     {
         /// <summary>
         /// Initializes a new instance of the computeLogin class.
@@ -65,105 +68,6 @@ namespace ComputeCS.Grasshopper
             pManager.AddTextParameter("Output", "Output", "Output", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            string auth = null;
-            string projectName = null;
-            int? projectNumber = null;
-            string taskName = null;
-            string overrides = null;
-            var create = false;
-
-            if (!DA.GetData(0, ref auth)) return;
-            if (!DA.GetData(1, ref projectName)) return;
-            DA.GetData(2, ref projectNumber);
-            if (!DA.GetData(3, ref taskName)) return;
-            DA.GetData(4, ref overrides);
-            DA.GetData(5, ref create);
-
-            ValidateName(taskName);
-            ValidateName(projectName);
-
-            // Get Cache to see if we already did this
-            var cacheKey = projectName + taskName + overrides;
-            var cachedValues = StringCache.getCache(cacheKey);
-            DA.DisableGapLogic();
-
-            if (cachedValues == null || create)
-            {
-                var queueName = "ProjectAndTask" + cacheKey;
-
-                // Get queue lock
-                var queueLock = StringCache.getCache(queueName);
-                if (queueLock != "true")
-                {
-                    StringCache.setCache(queueName, "true");
-                    StringCache.setCache(cacheKey, null);
-                    QueueManager.addToQueue(queueName, () =>
-                    {
-                        try
-                        {
-                            var results = ProjectAndTask.GetOrCreate(
-                                auth,
-                                projectName,
-                                projectNumber,
-                                taskName,
-                                overrides,
-                                create
-                            );
-                            cachedValues = results;
-                            StringCache.setCache(cacheKey, cachedValues);
-                            StringCache.setCache(this.InstanceGuid.ToString(), "");
-                            if (create)
-                            {
-                                StringCache.setCache(cacheKey + "create", "true");
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            StringCache.setCache(InstanceGuid.ToString(), e.Message);
-                            StringCache.setCache(cacheKey, "error");
-                            StringCache.setCache(cacheKey + "create", "");
-                        }
-
-                        ExpireSolutionThreadSafe(true);
-                        Thread.Sleep(2000);
-                        StringCache.setCache(queueName, "");
-                    });
-                }
-            }
-
-            // Read from Cache
-            if (cachedValues != null)
-            {
-                var outputs = cachedValues;
-                DA.SetData(0, outputs);
-                Message = "";
-                if (StringCache.getCache(cacheKey + "create") == "true")
-                {
-                    Message = "Task Created";
-                }
-            }
-
-            // Handle Errors
-            var errors = StringCache.getCache(InstanceGuid.ToString());
-            if (!string.IsNullOrEmpty(errors))
-            {
-                var messageLevel = GH_RuntimeMessageLevel.Error;
-                if (errors.Contains("No object found"))
-                {
-                    errors = "Could not find the desired project. Click create to create a new project.";
-                    messageLevel = GH_RuntimeMessageLevel.Warning;
-                }
-
-                AddRuntimeMessage(messageLevel, errors);
-            }
-        }
-
         private void ValidateName(string name)
         {
             var illegalCharacters = new List<string> { "?", "&", "/", "%", "#", "!", "+" };
@@ -176,6 +80,50 @@ namespace ComputeCS.Grasshopper
             }
         }
 
+        public override Task<ProjectTaskResult> CreateTask(IGH_DataAccess DA)
+        {
+            string auth = null;
+            string projectName = null;
+            int? projectNumber = null;
+            string taskName = null;
+            string overrides = null;
+            var create = false;
+
+            if (!DA.GetData(0, ref auth)) return DefaultTask();
+            if (!DA.GetData(1, ref projectName)) return DefaultTask();
+            DA.GetData(2, ref projectNumber);
+            if (!DA.GetData(3, ref taskName)) return DefaultTask();
+            DA.GetData(4, ref overrides);
+            DA.GetData(5, ref create);
+
+            ValidateName(taskName);
+            ValidateName(projectName);
+
+            return Task.Run(() =>
+                GetProjectTaskResult(auth, projectName, projectNumber, taskName, overrides, create));
+        }
+        public override void SetOutputData(IGH_DataAccess DA, ProjectTaskResult result)
+        {
+            if (result.Errors != null)
+            {
+                var errorMessage = result.Errors.Message;
+                var messageLevel = GH_RuntimeMessageLevel.Error;
+                if (result.Errors.Message.Contains("No object found"))
+                {
+                    errorMessage = "Could not find the desired project. Click create to create a new project.";
+                    messageLevel = GH_RuntimeMessageLevel.Warning;
+                }
+                AddRuntimeMessage(messageLevel, errorMessage);
+                return;
+            }
+
+            var create = false;
+            DA.GetData(5, ref create);
+            Message = create ? "Task Created" : "";
+
+            DA.SetData(0, result.Value);
+        }
+
 
         /// <summary>
         /// Provides an Icon for the component.
@@ -186,5 +134,28 @@ namespace ComputeCS.Grasshopper
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
         public override Guid ComponentGuid => new Guid("74e6ee44-9879-4769-83bb-3f2cdeb8dd7a");
+
+        private static ProjectTaskResult GetProjectTaskResult(string auth, string projectName, int? projectNumber, string taskName, string overrides, bool create)
+        {
+            try
+            {
+                return new ProjectTaskResult
+                {
+                    Value = ProjectAndTask.GetOrCreate(
+                        auth,
+                        projectName,
+                        projectNumber,
+                        taskName,
+                        overrides,
+                        create
+                    )
+                };
+            }
+            catch (Exception e)
+            {
+                return new ProjectTaskResult { Errors = e };
+            }
+        }
+
     }
 }
